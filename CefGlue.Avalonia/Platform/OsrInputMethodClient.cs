df22d6caa4e1e9eb3395547f8462bbfd21305e40
Preknight <1073530029@qq.com>
2024-10-23 20:13:59 +0800

    fix keyboard focus and ime support

---
+++ CefGlue.Avalonia/Platform/OsrInputMethodClient.cs
@@ -0,0 +1,44 @@
+﻿using Avalonia;
+using Avalonia.Controls.Presenters;
+using Avalonia.Controls;
+using Avalonia.Input.TextInput;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.VisualTree;
+using Avalonia.Input;
+
+namespace Xilium.CefGlue.Avalonia.Platform
+{
+    internal class OsrInputMethodClient : TextInputMethodClient
+    {
+        private ITextInputMethodImpl _im;
+        private Visual _presenter;
+        public override Visual TextViewVisual => _presenter!;
+
+        public override bool SupportsPreedit => true;
+
+        public override bool SupportsSurroundingText => true;
+
+        public override string SurroundingText => "";
+
+        public override Rect CursorRectangle => default;
+
+        public override TextSelection Selection { get; set; }
+
+        public void SetPresenter(Visual? presenter)
+        {
+            _presenter = presenter;
+            if (presenter != null)
+            {
+
+                _im = (TopLevel.GetTopLevel(_presenter) as ITextInputMethodRoot)?.InputMethod;
+                
+            }
+            else {  _im = null; }
+            
+        }
+    }
+}
---
+++ CefGlue.Demo.Avalonia/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Avalonia;
+using Xilium.CefGlue.Common;
+using Xilium.CefGlue.Common.Shared;
+
+namespace Xilium.CefGlue.Demo.Avalonia
+{
+    class Program
+    {
+
+        static int Main(string[] args)
+        {
+            AppBuilder.Configure<App>()
+                      .UsePlatformDetect()
+                      .With(new Win32PlatformOptions()
+                      {
+                          // CompositionMode = new [] { Win32CompositionMode.WinUIComposition }
+                      })
+                      .AfterSetup(_ => CefRuntimeLoader.Initialize(new CefSettings() {
+#if WINDOWLESS
+                          WindowlessRenderingEnabled = true
+#else
+                          WindowlessRenderingEnabled = false
+#endif
+                          ,
+                          //BackgroundColor = new CefColor(0x00, 0xff, 0xff, 0xff),
+                          Locale="zh-CN",
+
+                      },
+                      flags:new Dictionary<string, string> {
+                          {"--ignore-urlfetcher-cert-requests", "1" },
+                          {"--ignore-certificate-errors", "1" },
+                          {"--disable-web-security", "1" }
+                      }.ToArray(),
+                      customSchemes: new[] {
+                        new CustomScheme()
+                        {
+                            SchemeName = "test",
+                            SchemeHandlerFactory = new CustomSchemeHandler()
+                        }
+                      }))
+                      .StartWithClassicDesktopLifetime(args);
+                      
+            return 0;
+        }
+    }
+}
